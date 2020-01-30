5689089a792503b86495e9c7a027bdd20eab9ccb
Denis <dendiesel88@yandex.ru>
2020-01-30 22:59:31 +0300

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,39 @@
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class Main
+{
+    private static final Pattern SPACE = Pattern.compile("[^a-zA-zа-я-А-Я][ ]{0,}");
+
+    public static void main(String[] args)
+    {
+
+        if (args.length !=2)
+        {
+            System.err.println("Usage: JavaWordCount <in_file> <out_file>");
+            System.exit(1);
+        }
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("JavaWordCount")
+                .config("spark.master", "local")
+                .getOrCreate();
+
+        JavaRDD<String> lines = spark.read().textFile(args[0]).javaRDD();
+
+        JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());
+
+        JavaPairRDD<String, Integer> ones = words.mapToPair(s -> new Tuple2<>(s.toLowerCase(), 1));
+
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey((i1, i2) -> i1 + i2);
+
+        counts.saveAsTextFile(args[1]);
+        spark.stop();
+    }
+
+}
